b028e3424beea8e927ab4ea76c56655c6d32b1c3
Saitcan TAKILAN <saitcan.takilan@dijitalsahne.com>
2024-01-06 20:15:09 +0300

    Initial project files

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# ASP.NET Media Generator AI
+
+Bu repo, ASP.NET Core ile Pexels API kullanarak video ve fotoğraflar oluşturmanızda yardımcı olur.
+
+Beğendiyseniz daha fazla insana ulaşması için paylaşmayı unutmayın.
+
+Sevgiler.
---
+++ WebAPI/Controllers/MediaGeneratorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PexelsDotNetSDK.Api;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaGeneratorController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public MediaGeneratorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("Videos")]
+        [HttpGet]
+        public async Task<IActionResult> GenerateVideos(string search)
+        {
+            var client = new PexelsClient(_configuration["PexelsApiToken"]);
+            var searchVideos = client.SearchVideosAsync(search);
+            List<string> result = searchVideos.Result.videos.Select(s => s.videoFiles.FirstOrDefault().link).ToList();
+            return Ok(result);
+        }
+
+        [Route("Photos")]
+        [HttpGet]
+        public async Task<IActionResult> GeneratePhotos(string search)
+        {
+            var client = new PexelsClient(_configuration["PexelsApiToken"]);
+            var searchVideos = client.SearchPhotosAsync(search);
+            List<string> result = searchVideos.Result.photos.Select(s => s.source.original).ToList();
+            return Ok(result);
+        }
+    }
+}
